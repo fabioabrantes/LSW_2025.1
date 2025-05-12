676dc266ef88702a5570c1a68203f8cd0800f184
fabioabrantes <fabio.abrantes.diniz@gmail.com>
2025-05-12 13:55:25 -0300

    function updated

--- funtion.js
+++ funtion.js
@@ -16,6 +16,7 @@ let namesUsers = ["fabio", "jose", 5];
 const resultado = listUser(undefined, namesUsers, writerMessage);
 console.log(resultado);
 
+let frase = "jose é anonimo"
 function registerUser(user, idade) {
   let frase = "ola usuario ";
 
@@ -32,5 +33,53 @@ function registerUser(user, idade) {
   frase = frase + user;
   writerMessage2(frase);
 }
-
 registerUser("fabio", 16);
+
+// funções aninhadas - retornando a função interna
+function externa() {
+  let nome= "fabio"
+  function interna() {
+    console.log("Função interna executada",nome);
+  }
+
+  return interna; // retorna a referência da função
+}
+const chamar = externa(); 
+chamar(); 
+
+// Tornando a função interna acessível via objeto
+function externa2() {
+  function interna() {
+    console.log("Interna acessada via objeto");
+  }
+
+  return {
+    chamarInterna: interna
+  };
+}
+const obj = externa2();
+obj.chamarInterna();
+
+function contador(){
+  let numero = 0;
+
+  function incrementar(){
+    numero = numero + 1;
+    return numero;
+  }
+
+  return incrementar;
+}
+const meuIncrementador = contador();
+console.log(meuIncrementador());
+console.log(meuIncrementador());
+console.log(meuIncrementador());
+console.log(meuIncrementador());
+
+
+function showUsers(users="default"){
+
+  return console.log(users);
+}
+showUsers()
+showUsers(["fabio","jose"]);
